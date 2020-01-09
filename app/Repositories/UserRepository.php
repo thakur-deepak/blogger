6d322822136dcb5209ca8adc878341e39aa0d2fc
jsucreate <jatindersingh@ucreate.co.in>
2020-01-09 10:54:57 +0530

    Add reopsitory functions

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -5,8 +5,20 @@ use App\User;
 
 class UserRepository implements UserRepositoryInterface
 {
+    private $user;
+    
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+    
     public function store($request)
     {
-        return User::create($request->all());
+        return $this->user->create($request->all());
+    }
+
+    public function get()
+    {
+        return $this->user->get();
     }
 }
--- app/Repositories/UserRepositoryInterface.php
+++ app/Repositories/UserRepositoryInterface.php
@@ -5,4 +5,5 @@ namespace App\Repositories;
 interface UserRepositoryInterface
 {
     public function store(array $data);
+    public function get();
 }
