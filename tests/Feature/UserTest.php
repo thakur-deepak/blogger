8b72d2b1819e2da45deec04496b03543b621bda5
jsucreate <jatindersingh@ucreate.co.in>
2020-01-13 13:07:59 +0530

    update compser file

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -29,7 +29,7 @@ class UserTest extends TestCase
             'password' =>  $this->faker->password()
         ];
         $this->post($this->api_path. 'users', $attributes)
-            ->assertStatus(200)
+            ->assertStatus(201)
             ->assertSee('sucess');
     }
 }
