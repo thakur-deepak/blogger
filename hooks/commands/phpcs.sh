1a9f7d9536084c7c30c84b12072432107b0dc4d7
jsucreate <jatindersingh@ucreate.co.in>
2020-01-13 13:15:36 +0530

    update hook file

--- hooks/commands/phpcs.sh
+++ hooks/commands/phpcs.sh
@@ -5,4 +5,4 @@
 ./vendor/bin/phpcs --config-set colors 1 >> /dev/null
 ./vendor/bin/phpcs --config-set show_progress 1 >> /dev/null
 ./vendor/bin/phpcs --config-set show_warnings 1 >> /dev/null
-./vendor/bin/phpcs app/* -n --runtime-set ignore_warnings_on_exit true --standard=./.deploy/phpcs_ruleset.xml
+./vendor/bin/phpcs app/* -n --runtime-set ignore_warnings_on_exit true --standard=./hooks/rules/phpcs_ruleset.xml
--- hooks/commands/phpmd.sh
+++ hooks/commands/phpmd.sh
@@ -2,7 +2,7 @@
 # Lumen boilerplate githook script
 
 # PHP Mess Detector
-./vendor/bin/phpmd . text .deploy/phpmd_ruleset.xml --exclude vendor,_ide_helper.php,database,app/Console/Kernel.php,tests
+./vendor/bin/phpmd . text hooks/rules/phpmd_ruleset.xml --exclude vendor,_ide_helper.php,database,app/Console/Kernel.php,tests
 RESULT=$?
 [[ $RESULT -eq 0 ]] && echo "PHP Mess Detector check passed"
 
