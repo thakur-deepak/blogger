30330bc53d8d79afe3d3d9900b6d24968b9956bf
jsucreate <jatindersingh@ucreate.co.in>
2020-01-16 10:48:41 +0530

    Test uat deploy

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
- */
-
 Route::group(['prefix' => config('constants.API_PREFIX'), 'middleware' => 'CheckHeaders'], function ()
 {
     Route::post('/users', 'UsersController@store');
