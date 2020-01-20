0565ff94f63b9378f889a4dbf5bbad72143cf665
jsucreate <jatindersingh@ucreate.co.in>
2020-01-20 10:15:38 +0530

    Route testing

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,6 @@ Route::get('/test1', function () {
 });
 Route::group(['prefix' => config('constants.API_PREFIX'), 'middleware' => 'CheckHeaders'], function ()
 {
-    Route::get('/test2', function () {
-        return 'test2';
-    });
     Route::post('/users', 'UsersController@store');
     Route::group(['middleware' => 'Token'], function()
     {
