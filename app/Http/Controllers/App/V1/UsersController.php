1af2394dcfc38b34705c0891f117599cb888b936
Jatinder Singh <jatindersingh@ucreate.co.in>
2020-01-28 13:09:39 +0530

    test to remove validations

--- app/Http/Controllers/App/V1/UsersController.php
+++ app/Http/Controllers/App/V1/UsersController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\UserStoreRequest;
 use App\Repositories\UserRepositoryInterface;
 use Illuminate\Http\Request;
 
+
 class UsersController extends Controller
 {
     protected $user;
